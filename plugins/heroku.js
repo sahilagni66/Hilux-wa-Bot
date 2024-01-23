592cbeee8bb62c73f232d93b45cd28195f1a7f71
ERRORS <102719170+S-U-P-E-R-I-O-R@users.noreply.github.com>
2024-01-23 08:27:53 +0530

    Create heroku.js

---
+++ plugins/heroku.js
@@ -0,0 +1,193 @@
+
+const got = require("got");
+const Heroku = require("heroku-client");
+const {
+        version
+} = require("../package.json");
+const {
+	System,
+	isPrivate,
+	tiny
+} = require("../lib/");
+const Config = require("../config");
+const {
+	SUDO
+} = require("../config");
+const heroku = new Heroku({
+	token: Config.HEROKU_API_KEY
+});
+const baseURI = "/apps/" + Config.HEROKU_APP_NAME;
+const simpleGit = require("simple-git");
+const {
+	secondsToDHMS
+} = require("../lib");
+const git = simpleGit();
+const exec = require("child_process").exec;
+
+
+
+System({
+    pattern: "shutdown",
+    fromMe: true,
+    type: "heroku",
+    desc: "Dyno off",
+    type: "heroku",
+}, async (message) => {
+    await heroku.get(baseURI + "/formation").then(async (formation) => {
+        await message.send(` *-Hilux is shutting down-* `);
+        await heroku.patch(baseURI + "/formation/" + formation[0].id, {
+            body: { quantity: 0 },
+        });
+    })
+    .catch(async (error) => {
+        await message.send(`HEROKU: ${error.body.message}`);
+    });
+});
+
+
+
+
+System({
+    pattern: "setvar ",
+    fromMe: true,
+    type: "heroku",
+    desc: "Set Heroku environment variable",
+},
+async (message, match) => {
+    if (!match)
+        return await message.send(`Example: .setvar SUDO:918891005819`);   
+    const key = match.slice(0, match.indexOf(':')).trim();
+    const value = match.slice(match.indexOf(':') + 1).trim();
+    if (!key || !value)
+        return await message.send(`Example: .setvar SUDO:918891005819`);
+    heroku.patch(baseURI + "/config-vars", {
+            body: {
+                [key.toUpperCase()]: value,
+            },
+        })
+        .then(async () => {
+            await message.send(`${key.toUpperCase()}: ${value}`);
+        })
+        .catch(async (error) => {
+            await message.send(`HEROKU: ${error.body.message}`);
+        });
+});
+
+
+System({
+    pattern: "delvar ",
+    fromMe: true,
+    type: "heroku",
+    desc: "Delete Heroku environment variable",
+},
+async (message, match) => {
+    if (!match) return await message.send("_Example: delvar sudo_");
+    heroku
+        .get(baseURI + "/config-vars")
+        .then(async (vars) => {
+            const key = match.trim().toUpperCase();
+
+            if (vars[key]) {
+                await heroku.patch(baseURI + "/config-vars", {
+                    body: {
+                        [key]: null,
+                    },
+                });
+
+                return await message.send(`_Deleted ${key}_`);
+            }
+
+            await message.send(`_${key} not found_`);
+        })
+        .catch(async (error) => {
+            await message.send(`*HEROKU: ${error.body.message}*`);
+        });
+});
+
+
+System({
+    pattern: "allvar",
+    fromMe: true,
+    type: "heroku",
+    desc: "Heroku all environment variables",
+},
+async (message) => {
+    let msg = "Here are all your Heroku vars\n\n\n";
+
+    try {
+        const keys = await heroku.get(baseURI + "/config-vars");
+
+        for (const key in keys) {
+            msg += `${key} : ${keys[key]}\n\n`;
+        }
+
+        await message.send(msg + "");
+    } catch (error) {
+        await message.send(`HEROKU: ${error.message}`);
+    }
+});
+
+
+System({
+    pattern: "update",
+    fromMe: true,
+    type: "heroku",
+    desc: "Checks for update.",
+},
+async (message, match) => {
+    let { prefix } = message;
+
+    if (match === "now") {
+        await git.fetch();
+        var commits = await git.log([
+            Config.BRANCH + "..origin/" + Config.BRANCH,
+        ]);
+
+        if (commits.total === 0) {
+            return await message.send(`_Hilux on the latest version: v${version}_`);
+        } else {
+            await message.reply("  -Hilux is updating- ");
+
+            try {
+                var app = await heroku.get("/apps/" + Config.HEROKU_APP_NAME);
+            } catch {
+                await message.send("_Invalid Heroku Details_");
+                await new Promise((r) => setTimeout(r, 1000));
+            }
+
+            git.fetch("upstream", Config.BRANCH);
+            git.reset("hard", ["FETCH_HEAD"]);
+
+            var git_url = app.git_url.replace(
+                "https://",
+                "https://api:" + Config.HEROKU_API_KEY + "@"
+            );
+
+            try {
+                await git.addRemote("heroku", git_url);
+            } catch {
+                console.log("heroku remote error");
+            }
+
+            await git.push("heroku", Config.BRANCH);
+            await message.send("  _JARVIS IS UPDATED_");
+        }
+    }
+
+    await git.fetch();
+    var commits = await git.log([Config.BRANCH + "..origin/" + Config.BRANCH]);
+
+    if (commits.total === 0) {
+        await message.send("_Already on the latest version_");
+    } else {
+        var availupdate = "*𝘜𝘱𝘥𝘢𝘵𝘦𝘴 𝘈𝘳𝘦 𝘈𝘷𝘪𝘭𝘢𝘣𝘭𝘦* \n\n";
+
+        commits["all"].map((commit, num) => {
+            availupdate += num + 1 + " * " + tiny(commit.message) + "\n";
+        });
+
+        return await message.client.sendMessage(message.jid, {
+            text: `${availupdate}\n\n _type *${Config.HANDLERS} update now*_`
+        });
+    }
+});
